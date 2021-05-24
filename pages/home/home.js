e63f0dac21758cf84f71b3d82dde0cf5eb82e2f5
yuanmin <yuanminpersonal@163.com>
2021-05-24 17:57:01 +0800

    小程序初体验

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,62 +5,33 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    name: 'YM',
+    age: 18,
+    students: [
+      {id: 110, name: 'kobe', age: 30},
+      {id: 111, name: 'kuli', age: 31},
+      {id: 112, name: 'james', age: 35}
+    ],
+    counter: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleBtnClick() {
+    // 错误做法
+    // this.data.counter++
+    // console.log(this.data.counter)
+    // this.setData()
+    this.setData({
+      counter: this.data.counter + 1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleSubtraction() {
+    this.setData({
+      counter: this.data.counter - 1
+    })
   }
+
+  
 })
+
+// 编程范式：
+// 1.命令式编程：原生操作dom
+// 2.声明式编程：Vue/React/Angular
